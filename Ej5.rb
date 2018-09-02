38a940d3b6aa44a94a8b12a96df2c5e54af7677d
Khriz <guerracritofer@gmail.com>
2018-09-02 16:37:50 -0600

    ejercicio?5

---
+++ Ej5.rb
@@ -0,0 +1,11 @@
+puts 'ingrese numero'
+num = gets.to_i
+if num / 3==0
+    puts 'Pling'
+elsif num / 5==0
+        puts 'Plang'
+elsif num/7==0 
+     puts 'Plong'
+    else 
+     puts "#{num}"
+    end
